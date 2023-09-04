766a3f46474fe58a54b5d4fa14af03a21fa70b87
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-09-04 09:35:11 -0300

    Iniciando a modularização no projeto Boyler-Plate.

---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,13 @@
+export default function Conteudo(){
+
+    return(
+        <section>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis molestias adipisci placeat rem odio illo pariatur aut minima aliquam, odit, optio blanditiis mollitia earum enim autem illum fugiat sapiente voluptates.</p>
+                    <p>Voluptatibus a earum recusandae? Eligendi magnam, natus dolorem qui dolores repellat aspernatur porro vitae sint ut? Molestiae deleniti, consectetur eum ab eligendi delectus modi exercitationem. Non voluptas dolorum optio commodi.</p>
+                    <p>Est, fugit voluptatum id corporis autem quasi minima, totam quo non ipsam similique minus molestias possimus, libero ullam eaque maxime saepe quas? At, delectus optio sapiente aut aperiam et voluptas!</p>
+                    <img src="./assets/react.svg" alt="Logo React" />
+                </div>
+        </section>
+    )
+}
---
+++ src/components/Rodape.jsx
@@ -0,0 +1,15 @@
+export default function Rodape(){
+
+    return(
+        <footer>
+                <div>
+                    <ul>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                    <p>&copy; Todos os meus direitos reservados. 2023</p>
+                </div>
+        </footer> 
+    )
+}
