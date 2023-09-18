a43c9f71b7fe8dc3a815be0216196ca759ea9cb5
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-09-18 09:09:25 -0300

    Editando os componentes.

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import Cabecalho from "./components/Cabecalho";
 import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
 import viteLogo from "./assets/vite.svg";
+import style from "./App.module.css";
 
 export default function App(){
     let viteLogoAlt = 'vite logo'
     return(
       <>
-        <h1>APP - Componente principal!</h1>
-        <div>
+        <h1 className={style.titulo}>APP - Componente principal!</h1>
+        <div className={style.container}>
 
         {/* Iniciando a área do cabeçalho */}
         <Cabecalho/>    
 
         {/* Iniciando a área do conteúdo */}
-        <Conteudo VitelogoProps={viteLogo} viteLogoAltProps={viteLogoAlt}/>
+        <Conteudo viteLogoProps={viteLogo} viteLogoAltProps={viteLogoAlt}/>
 
         {/* Iniciando a área do rodapé */}
         <Rodape/>
