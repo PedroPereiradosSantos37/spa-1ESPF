de89b50aab48d7158abc4d66beb5adeee0a26cee
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-10-09 09:59:29 -0300

    2ª forma de utilizar o input com useState

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -7,20 +7,34 @@ export default function EditarProdutos() {
 
     document.title = "Editar Produtos " + id; 
 
-    const[produto, setProduto] = useState({});
+    const[produto,setProduto] = useState({
+    });
 
     //Criar uma estratégia para recuperar o produto da API-JSON com fetch, utilizando GET:
-
     useEffect(()=>{
-
+      
       fetch(`http://localhost:5000/produtos/${id}`)
-      .then((response)=>response.json())
-      .then((response)=>  setProduto(response))
-      .catch(error=>console.log(error));
+      .then((response)=> response.json())
+      .then((response)=> setProduto(response))
+      .catch(error=> console.log(error));
 
     },[id]);
     
-    
+    const handleChange = (e)=>{
+
+      //Destructuring
+      const {name,value} = e.target;
+
+      if(name == "nome"){
+        setProduto({"nome":value,"desc":"","preco":""});
+      }else if(name == "desc"){
+        setProduto({"nome":"","desc":value,"preco":""});
+      }else if(name == "preco"){
+        setProduto({"nome":"","desc":"","preco":value});
+      }
+      
+    }
+
 
   return (
     <div>
@@ -31,15 +45,15 @@ export default function EditarProdutos() {
                 <legend>Produto Selecionado</legend>
                 <div>
                   <label htmlFor="">Nome:</label>
-                  <input type="text" name="nome" placeholder="Digite o nome do Produto." value= {produto.nome} onChange={(e)=> setProduto(e.target.value)}/>
+                  <input type="text" name="nome" placeholder="Digite o nome do Produto." value={produto.nome} onChange={handleChange}/>
                 </div>
                 <div>
                   <label htmlFor="">Descrição:</label>
-                  <input type="text" name="desc" placeholder="Digite a descrição do Produto." value= {produto.desc} onChange={(e)=> setProduto(e.target.value)}/>
+                  <input type="text" name="desc" placeholder="Digite a descrição do Produto." value={produto.desc} onChange={handleChange}/>
                 </div>
                 <div>
                   <label htmlFor="">Preço:</label>
-                  <input type="text" name="preco" placeholder="Digite o preço do Produto." value= {produto.preco} onChange={(e)=> setProduto(e.target.value)}/>
+                  <input type="text" name="preco" placeholder="Digite o preço do Produto." value={produto.preco} onChange={handleChange}/>
                 </div>
                 <div>
                   <button>EDITAR</button>
@@ -51,7 +65,6 @@ export default function EditarProdutos() {
     </div>
   )
 }
-
 
 // //1ª FORMA DE INPUT COM useState
 // <form>
